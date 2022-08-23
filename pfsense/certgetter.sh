1e00d7f0dc4cb8aa7bb1ba8eb94684eaa6594ccd
Ezequiel Lage <ezequiel@lageteck.com>
2022-08-23 16:17:30 -0300

    Adding file for test download

---
+++ pfsense/certgetter.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo
+########################################
+#              CertGetter              #
+########################################
+# Developed by Ezequiel Lage (@ezlage) #
+# Sponsored by Lageteck (lageteck.com) #
+########################################
+echo
+echo "This file version is for test download only"
